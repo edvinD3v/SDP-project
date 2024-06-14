f19b5518ab40e482b3195f156fd04f597e726f5a
Edvin225883 <edvin.obralija@stu.ibu.edu.ba>
2024-06-14 17:48:50 +0200

    setings modal

--- Matemal/screens/mainScreen.tsx
+++ Matemal/screens/mainScreen.tsx
@@ -1,13 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { ImageBackground, StyleSheet, Text, View } from 'react-native';
+import { ImageBackground, StyleSheet, Text, View, Pressable, Modal } from 'react-native';
 import * as ScreenOrientation from 'expo-screen-orientation';
-import React, { useEffect } from 'react';
 import Gameicon from '../components/gameIcon';
+import React, { useState } from 'react';
 
 export default function MainScreen() {
+  const [isModalVisible, setIsModalVisible] = useState(false);
 
   return (
     <ImageBackground source={require('../assets/backgrounds/bg1.jpg')} style={styles.backgroundImage}>
+    <Pressable style={styles.settingsButton} onPress={() => setIsModalVisible(true)}>
+      <ImageBackground source={require('../assets/buttonIcons/settings.png')} style={styles.backgroundImageSettings}></ImageBackground>
+    </Pressable>
     <View style={styles.gameChooseTable}>
       <View style={styles.tableRow}>
         <Gameicon title={'Counting'}></Gameicon>
@@ -23,6 +27,23 @@ export default function MainScreen() {
         <Gameicon title={'Quiz'}></Gameicon>
       </View>
     </View>
+    <Modal transparent={true} visible={isModalVisible} onRequestClose={() => setIsModalVisible(false)}>
+      <View style={styles.modalContainer}>
+        <View style={styles.modal}>
+          <Text style={styles.settingsLabel}>Settings</Text>
+          <View style={styles.settingRow}>
+            <Text style={styles.settingName}>Language: </Text>
+          </View>
+
+          <View style={styles.settingRow}>
+            <Text style={styles.settingName}>Sound: </Text>
+          </View>
+
+        </View>
+
+      </View>
+
+    </Modal>
   </ImageBackground>
   );
 }
@@ -41,8 +62,25 @@ const styles = StyleSheet.create({
     width: '100%',
     height: '100%'
   },
+  backgroundImageSettings: {
+    width: '100%',
+    height: '100%',
+  },
+  settingsButton: {
+    backgroundColor: "#00ABF0",
+    margin: 10,
+    padding: 8,
+    width: 70,
+    height: 70,
+    borderRadius: 15,
+    borderWidth: 3,
+    borderColor: "white",
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
   gameChooseTable: {
     marginLeft: '40%',
+    marginTop: -60,
     margin: 10,
     height: '80%',
     alignItems: 'center'
@@ -50,5 +88,34 @@ const styles = StyleSheet.create({
   tableRow: {
     flexDirection: 'row',
     marginBottom: 12
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+  modal: {
+    width: '60%',
+    height: '85%',
+    padding: 20,
+    backgroundColor: '#16744f',
+    borderRadius: 10,
+    borderWidth: 13,
+    borderColor: '#f18a3d',
+    alignItems: 'center',
+  },
+  settingsLabel: {
+    fontSize: 40,
+    fontWeight: '700',
+    color: 'white'
+  },
+  settingRow: {
+    flexDirection: 'row',
+    marginTop: 30,
+  },
+  settingName: {
+    color: 'white',
+    fontSize: 25,
   }
 });
